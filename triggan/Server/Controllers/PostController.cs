392f5d6718d60a4ccf6e7fc435f303eefdb2c21f
Tristan <myndmunnd@gmail.com>
2020-09-27 00:28:26 +0200

    Add blog list (use api controller and blazor component) Change blogcontroller to postcontroller Update CSS with soft blue colors
    Task #85

--- triggan/Server/Controllers/PostController.cs
+++ triggan/Server/Controllers/PostController.cs
@@ -10,11 +10,11 @@ namespace triggan.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class BlogController : ControllerBase
+    public class PostController : ControllerBase
     {   
-        private readonly ILogger<BlogController> _logger;
+        private readonly ILogger<PostController> _logger;
 
-        public BlogController(ILogger<BlogController> logger)
+        public PostController(ILogger<PostController> logger)
         {
             _logger = logger;
         }
@@ -28,7 +28,7 @@ namespace triggan.Server.Controllers
                 {
                     Title = "First Post",
                     Content = "This is the first blog post with default content",
-                    Excerpt = "This is the first blog post with default Excerpt",
+                    Excerpt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a commodo ex. Etiam tempor lorem mattis risus convallis rhoncus. Nam in aliquet orci, id accumsan magna. Duis velit lectus, ullamcorper sit amet lacus eu, viverra efficitur quam.",
                     CoverImagePath = "https://28oa9i1t08037ue3m1l0i861-wpengine.netdna-ssl.com/wp-content/uploads/2020/09/FEATURE-site-782x530.png",
                     Public = true,
                     PublicationDate = DateTime.Now,
