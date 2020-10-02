91257078aa7dee22baddad78a759feab8c0a8cc2
Tristan Robin <ext-tristan.robin@flowbird.group>
2020-10-02 16:09:28 +0200

    Add ContactForm Blazor component
    Task #102

---
+++ Data/Model/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+	public class Message
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Updated { get; set; } = DateTime.Now;
+        public string Name { get; set; }
+		public string EMail { get; set; }
+		public string Content { get; set; }
+	}
+}
