1e1358bfb42ee3b2824fa31ea0a29088108706a8
Tristan <myndmunnd@gmail.com>
2020-10-04 09:05:14 +0200

    Add ProjectController (mimics PostController's behavior) Add ProjectControllerTests, not run
    Task #90

---
+++ triggan/Server/Controllers/ProjectController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Model.Enums;
+using triggan.Interfaces;
+
+namespace triggan.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        private readonly ISlugRepository<Project> repository;
+
+        public ProjectController(ISlugRepository<Project> repo)
+        {
+            this.repository = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Project> Get()
+        {
+            return repository.Get();
+        }
+
+        [HttpGet("{slug}")]
+        public Project Get(string slug)
+        {
+            return repository.Get(slug);
+        }
+    }
+}
