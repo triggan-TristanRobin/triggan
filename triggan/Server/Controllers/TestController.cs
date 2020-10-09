efd002677e2b9cf83bf93b43db31131285136cc4
Tristan <myndmunnd@gmail.com>
2020-10-10 00:23:49 +0200

    Add test page and Controllers/TestController.cs     Currently set to test appsettings and secrets.

---
+++ triggan/Server/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+namespace triggan.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        public TestController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet("[action]")]
+        public string GetConfig()
+        {
+            return Configuration.GetSection("Test").GetSection("inner").Value;
+        }
+
+        [HttpGet("[action]")]
+        public string Get()
+        {
+            return "Hello there";
+        }
+    }
+}
