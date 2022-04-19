076f005359d5b270bfb3b358c6f43815a5a05657
Tristan Robin <tritan.robin69@gmail.com>
2022-04-19 21:48:18 +0200

    Reset about page to match learn/child intent. Fix few visuals

--- triggan.BlogModel/Message.cs
+++ triggan.BlogModel/Message.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace triggan.BlogModel
 {
     public class Message : Entity
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string EMail { get; set; }
+        [Required]
         public string Content { get; set; }
     }
 }
